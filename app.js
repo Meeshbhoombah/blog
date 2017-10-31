c4191e81de4de82f0664fc36bf7719c2ebfe2b3f
Rohan Mishra <rohanmishra2017@gmail.com>
2017-10-30 17:30:12 -0700

    Defined routes for posts

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var config = require('config');
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(config.database, config.username, config.password, { dialect: 'postgres' });
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var posts = require('./routes/posts')
 
 var app = express();
 
@@ -24,13 +28,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/posts', posts);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 // error handler
 app.use(function(err, req, res, next) {
